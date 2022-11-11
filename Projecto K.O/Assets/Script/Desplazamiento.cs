9f02edc65f75044845fd0fbbf0dcb81d00238b4b
Franco-Castillo <93136916+Franco-Castillo@users.noreply.github.com>
2022-11-11 18:55:31 -0300

    Actualizacion1
    Cambio de Movimiento Pero falta salto

---
+++ Projecto K.O/Assets/Script/Desplazamiento.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desplazamiento : MonoBehaviour
+{
+    public float velocidad;
+    private Rigidbody2D rigidbody;
+    private bool MDerecha = true;
+
+
+    private void Start()
+    {        
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ProcesarM();
+    }
+
+    void ProcesarM()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(inputMovimiento * velocidad, rigidbody.velocity.y);
+
+        GOrientacion(inputMovimiento);
+    }
+
+    void GOrientacion(float inputMovimiento)
+    {
+        if( (MDerecha == true && inputMovimiento <0) || (MDerecha == false && inputMovimiento > 0) )
+        {
+            MDerecha = !MDerecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+
+}
